6f8a79ebe2eba6ac43449cdc82e6f43990a9075a
Adrian WEAVER <aweaver@student.42.fr>
2022-12-06 16:43:35 +0100

    FIX CPP03 LEAKS:
    Fixing leaks, reworking exceptions in cpp03.

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 09:37:48 by aweaver           #+#    #+#             */
-/*   Updated: 2022/12/02 11:39:34 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/12/06 16:41:21 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,28 @@
 
 int main(void)
 {
+	AForm	*immunity = NULL;
+	AForm	*wesh = NULL;
+	AForm	*adventofcode = NULL;
 	std::cout << "Trying to execute a form that has not been signed:" << std::endl;
 	{
 		try
 		{
 			Bureaucrat			papa("Daddy", 10);
 			Intern				stagiaire;
-			AForm				*wesh;
 			wesh = stagiaire.makeForm("shrubbery creation", "jardin");
 			papa.executeForm(*wesh);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete wesh;
+		wesh = NULL;
 	}
 	std::cout << std::endl << "Trying to execute a signed form:" << std::endl;
 	{
@@ -56,31 +48,20 @@ int main(void)
 		{
 			Bureaucrat			papa("Daddy", 10);
 			Intern				stagiaire;
-			AForm				*wesh;
 			wesh = stagiaire.makeForm("shrubbery creation", "jardin");
 			(*wesh).beSigned(papa);
 			papa.executeForm(*wesh);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete wesh;
+		wesh = NULL;
 	}
 	std::cout << std::endl << "Trying to execute an unsigned RobotomyForm:" << std::endl;
 	{
@@ -89,30 +70,19 @@ int main(void)
 			Bureaucrat				afaure("Auguste", 10);
 			Bureaucrat				aweaver("aweaver", 42);
 			Intern					stagiaire;
-			AForm					*adventofcode;
 			adventofcode = stagiaire.makeForm("robotomy request", "aweaver");
 			aweaver.executeForm(*adventofcode);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete adventofcode;
+		adventofcode = NULL;
 	}
 	std::cout << std::endl << "Trying to execute a signed RobotomyForm:" << std::endl;
 	{
@@ -121,31 +91,20 @@ int main(void)
 			Bureaucrat				afaure("Auguste", 10);
 			Bureaucrat				aweaver("aweaver", 42);
 			Intern					stagiaire;
-			AForm					*adventofcode;
 			adventofcode = stagiaire.makeForm("robotomy request", "aweaver");
 			(*adventofcode).beSigned(afaure);
 			aweaver.executeForm(*adventofcode);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete adventofcode;
+		adventofcode = NULL;
 	}
 	std::cout << std::endl << "Trying to execute a signed PresidentialPardon with a low grade:" << std::endl;
 	{
@@ -154,31 +113,20 @@ int main(void)
 			Bureaucrat				calamity("calamity the almighty", 1);
 			Bureaucrat				bsavinel("bsavinel the weak", 150);
 			Intern					stagiaire;
-			AForm					*immunity;
 			immunity = stagiaire.makeForm("presidential pardon", "aweaver");
 			(*immunity).beSigned(calamity);
 			bsavinel.executeForm(*immunity);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete immunity;
+		immunity = NULL;
 	}
 	std::cout << std::endl << "Trying to execute a signed PresidentialPardon with a proper grade:" << std::endl;
 	{
@@ -186,31 +134,20 @@ int main(void)
 		{
 			Bureaucrat				calamity("calamity the almighty", 1);
 			Intern					stagiaire;
-			AForm					*immunity;
 			immunity = stagiaire.makeForm("presidential pardon", "aweaver");
 			(*immunity).beSigned(calamity);
 			calamity.executeForm(*immunity);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
 			std::cout << "caught: " << e.what() << std::endl;
 		}
+		delete immunity;
+		immunity = NULL;
 	}
 	std::cout << std::endl << "Tryin to create an invalid form:" << std::endl;
 	{
@@ -218,31 +155,20 @@ int main(void)
 		{
 			Bureaucrat				calamity("calamity the almighty", 1);
 			Intern					stagiaire;
-			AForm					*immunity;
 			immunity = stagiaire.makeForm("prepare the codingame fall challenge", "aweaver");
 			(*immunity).beSigned(calamity);
 			calamity.executeForm(*immunity);
 		}
-		catch (AForm::GradeTooHighException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::GradeTooLowException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
-		catch (AForm::FormNotSignedException &e)
-		{
-			std::cout << "caught: " << e.what() << std::endl;
-		}
 		catch (std::bad_alloc &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "allocation error: " << e.what() << std::endl;
 		}
-		catch (Intern::WrongFormException &e)
+		catch (std::exception &e)
 		{
-			std::cout << "caught: " << e.what() << std::endl;
+			std::cout << "caught" << e.what() << std::endl;
 		}
+			delete immunity;
+			immunity = NULL;
 	}
 	return (0);
 }
